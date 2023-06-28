f082746d0eebdd01526b015c6199e05973a5452e
Valeriya Mikhalskaya <mikhalskaya@amicaldo.de>
2023-06-28 20:46:01 +0200

    Add TaskMessage to Worker

--- src/main/java/com/example/Worker.java
+++ src/main/java/com/example/Worker.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
@@ -10,20 +11,37 @@ class Worker extends AbstractBehavior<Worker.Message> {
 
     public interface Message {};
 
-    private final int taskId;
+    //message from task
+    public record TaskMessage(ActorRef<Scheduler.Message> msgFrom, ActorRef<Task.Message> taskElmt, ActorRef<Task.Message> taskObj, int action) implements Message {  }
 
-    private Worker(ActorContext<Message> context, int taskId) {
-        super(context);
-        this.taskId = taskId;
+
+    public static Behavior<Message> create(ActorRef<Scheduler.Message> msgFrom, ActorRef<Task.Message> taskElmt, ActorRef<Task.Message> taskObj, int action) {
+        return Behaviors.setup(context -> action == 0 ? new Worker(context, msgFrom, taskElmt, taskObj, 0) : new Worker(context, msgFrom, taskElmt, taskObj, 1) );
     }
 
-    public static Behavior<Message> create() {
-        return Behaviors.setup(context -> new Worker(context, 0));
+    private final ActorRef<Scheduler.Message> msgFrom;
+    private final ActorRef<Task.Message> taskElmt;
+    private final ActorRef<Task.Message> taskObj;
+    private final int action;
+
+
+    private Worker(ActorContext<Message> context, ActorRef<Scheduler.Message> msgFrom, ActorRef<Task.Message> taskElmt, ActorRef<Task.Message> taskObj, int action) {
+        super(context);
+        this.msgFrom = msgFrom;
+        this.taskElmt = taskElmt;
+        this.taskObj = taskObj;
+        this.action = action;
     }
 
     @Override
     public Receive<Message> createReceive() {
         return newReceiveBuilder()
+                .onMessage(TaskMessage.class, this::onExampleMessage)
                 .build();
+    }
+
+    private Behavior<Message> onExampleMessage(TaskMessage msg) {
+        getContext().getLog().info("I got an action: {}", this.action);
+        return this;
     }
 }
